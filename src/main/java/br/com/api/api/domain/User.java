2c045928f69a63db4fcb2e4bf1bf63ff178b12d5
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-02-20 19:48:24 -0300

    first commit

---
+++ src/main/java/br/com/api/api/domain/User.java
@@ -0,0 +1,23 @@
+package br.com.api.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    private String senha;
+
+    @Column(unique = true)
+    private String email;
+}
